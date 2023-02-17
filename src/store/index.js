df5c3f3f91023c587315a380830ca821504d4590
Andrea Barghigiani <a.barghigiani@gmail.com>
2023-02-17 17:47:51 +0100

    feat: leveraged RTK Query

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,16 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import { citiesReducer, addCity, currentCity } from "./slices/cities";
-
-// Actions
+import { setupListeners } from "@reduxjs/toolkit/dist/query/react";
+import { weatherSlice } from "./slices/weatherApiSlice";
 
 const store = configureStore({
   reducer: {
     currentCity,
     cities: citiesReducer,
+    [weatherSlice.reducerPath]: weatherSlice.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(weatherSlice.middleware),
 });
 
 export { store, addCity, currentCity };
---
+++ src/store/slices/weatherApiSlice.js
@@ -0,0 +1,26 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const weatherSlice = createApi({
+  reducerPath: "weather",
+  baseQuery: fetchBaseQuery({
+    baseUrl: `https://api.openweathermap.org/data/3.0/`,
+  }),
+  endpoints: (builder) => ({
+    getWeather: builder.query({
+      query: (city) => {
+        const { lat, lon } = city.position;
+        return {
+          url: "onecall",
+          params: {
+            lat,
+            lon,
+            appid: import.meta.env.VITE_WEATHER_KEY,
+            units: "metric",
+          },
+        };
+      },
+    }),
+  }),
+});
+
+export const { useGetWeatherQuery } = weatherSlice;
